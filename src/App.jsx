43447d0c9e53292cebf502e5396bb67ebb906bfb
JONAS.RR <jonasarcuri2020@gmail.com>
2026-06-23 12:04:15 -0300

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
